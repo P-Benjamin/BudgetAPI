5928c5849aa3ac20024519e8b23420c68ef465b8
BenjaminPinosa <benjamin.pinosa@e.rascol.net>
2025-07-17 14:57:35 +0200

    add front

---
+++ BudgetAPI/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿namespace BudgetAPI.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Role { get; set; }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+    }
+
+    public class UserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
