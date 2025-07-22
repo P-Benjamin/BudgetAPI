875b076132f8760066d0514cc73df7396d19cc6d
Mégane <susini.megane04@gmail.com>
2025-07-22 19:28:42 +0200

    add validation for each class

--- BudgetAPI/Models/User.cs
+++ BudgetAPI/Models/User.cs
@@ -1,13 +1,33 @@
-﻿namespace BudgetAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Le nom d'utilisateur est obligatoire.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Le nom d'utilisateur doit contenir entre 3 et 50 caractères.")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères.")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "L'adresse email est obligatoire.")]
+        [EmailAddress(ErrorMessage = "Adresse email invalide.")]
         public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Le rôle est obligatoire.")]
+        [StringLength(20, ErrorMessage = "Le rôle ne doit pas dépasser 20 caractères.")]
         public string Role { get; set; }
+
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le nom ne doit pas dépasser 50 caractères.")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Le prénom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le prénom ne doit pas dépasser 50 caractères.")]
         public string GivenName { get; set; }
     }
 }
